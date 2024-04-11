c395f40fbd080218545f68443aa6db0296dd9867
rafah <rafah.hawa@yahoo.fr>
2024-04-11 23:14:08 +0200

    add admin parameters

--- clinic/admin.py
+++ clinic/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Doctor, Clinic, Gallery, PatientReview, BlogPost, LatestNews
+
+
+class GalleryInline(admin.TabularInline):
+    model = Gallery
+    extra = 1
+
+class ClinicAdmin(admin.ModelAdmin):
+    inlines = [GalleryInline]
+    list_display = ('name', 'address', 'phone_number', 'email', 'website', 'doctor')
+    search_fields = ('name', 'address', 'doctor__name')
+    
+admin.site.register(Doctor)
+admin.site.register(Clinic)
+admin.site.register(Gallery)
+admin.site.register(PatientReview)
+admin.site.register(BlogPost)
+admin.site.register(LatestNews)
--- clinic/views.py
+++ clinic/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Doctor, Clinic, Gallery, PatientReview, BlogPost, LatestNews
 
 # Create your views here.
