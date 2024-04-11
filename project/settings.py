689e5ef9cdffbe461fb18d7c067d3360e0f8dbfb
rafah <rafah.hawa@yahoo.fr>
2024-04-11 22:15:34 +0200

    add static media settings

--- project/settings.py
+++ project/settings.py
@@ -121,3 +121,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = "staticfiles"
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+    
+]
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/"media"
